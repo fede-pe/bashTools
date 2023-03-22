7476e6471b7e5387bf314481fdd3ee74c807b556
fede-pe <fepe.ig@gmail.com>
2023-03-22 16:37:22 +0100

    copoy remane program

---
+++ copyRename.sh
@@ -0,0 +1,17 @@
+# This bash script copies files from different folders with colliding names and rename them
+# Usage:
+# copyRename.sh fileNameRoot copyFilesQuery
+
+FILENAME_EXTENSION='.mrc';
+COUNTER=0;
+
+
+for FILE in "${@:2}";
+do
+	basename "$FILE";
+	f=$(basename -- "$FILE")
+	echo "$f";
+	echo "cp $FILE $1_$COUNTER$FILENAME_EXTENSION";
+	cp $FILE $1_$COUNTER$FILENAME_EXTENSION;
+	COUNTER=$[$COUNTER +1];
+done
