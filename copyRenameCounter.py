8d9141a9de95e1b34c041d03d968072cbe7c8bee
fede-pe <fepe.ig@gmail.com>
2023-05-29 16:35:35 +0200

    reformat copy rename to use regex

--- copyRenameCounter.py
+++ copyRenameCounter.py
@@ -1,29 +1,38 @@
 import os
+import re
 import shutil
 
 
-def copy_file_with_counter(source_file, destination_folder):
-    # Extract the basename and extension of the source file
-    basename, extension = os.path.splitext(os.path.basename(source_file))
-    counter = 1
+def copy_file_with_counter(source_file_regex, destination_folder):
+    # Get the list of files in the source directory
+    source_files = os.listdir()
 
-    # Generate the destination file path
-    destination_file = os.path.join(destination_folder, f"{basename}{extension}")
+    # Filter the files based on the provided regular expression
+    regex = re.compile(source_file_regex)
+    matching_files = [file for file in source_files if regex.match(file)]
 
-    # Check if the destination file already exists
-    while os.path.exists(destination_file):
-        # Append the counter to the basename and generate a new destination file path
-        basename_with_counter = f"{basename}_{counter}"
-        destination_file = os.path.join(destination_folder, f"{basename_with_counter}{extension}")
-        counter += 1
+    for source_file in matching_files:
+        # Extract the basename and extension of the source file
+        basename, extension = os.path.splitext(source_file)
+        counter = 1
 
-    # Copy the file to the destination folder
-    shutil.copy2(source_file, destination_file)
-    print(f"File '{source_file}' copied to '{destination_file}'.")
+        # Generate the destination file path
+        destination_file = os.path.join(destination_folder, f"{basename}{extension}")
+
+        # Check if the destination file already exists
+        while os.path.exists(destination_file):
+            # Append the counter to the basename and generate a new destination file path
+            basename_with_counter = f"{basename}_{counter}"
+            destination_file = os.path.join(destination_folder, f"{basename_with_counter}{extension}")
+            counter += 1
+
+        # Copy the file to the destination folder
+        shutil.copy2(source_file, destination_file)
+        print(f"File '{source_file}' copied to '{destination_file}'.")
 
 
-# Take source file and destination folder as input
-source_file = input("Enter the path of the source file: ")
+# Take source file regex and destination folder as input
+source_file_regex = input("Enter the regular expression for the source file(s): ")
 destination_folder = input("Enter the path of the destination folder: ")
 
-copy_file_with_counter(source_file, destination_folder)
+copy_file_with_counter(source_file_regex, destination_folder)
